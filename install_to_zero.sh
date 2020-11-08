95749e6a6540459dc67eac955fd469ca8dddf949
k-Knight <konstantin.spils@gmail.com>
2020-11-08 22:29:30 +0200

    removed redundant files

--- install_to_zero.sh
+++
@@ -1,3 +0,0 @@
-screen -X quit
-arduino-cli compile --fqbn Seeeduino:samd:zero --libraries="/home/odyssey/misc/arduino/libraries" fan_controller_zero
-arduino-cli upload --fqbn Seeeduino:samd:zero -p /dev/ttyACM0 fan_controller_zero

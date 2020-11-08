ce70324da688330a31c03a15f21402a9e8613944
k-Knight <28106261+k-Knight@users.noreply.github.com>
2020-11-08 22:29:30 +0200

    removed redundant files

--- install_to_uno.sh
+++
@@ -1,3 +0,0 @@
-screen -X quit
-arduino-cli compile --fqbn arduino:avr:uno --libraries="/home/odyssey/misc/arduino/libraries" fan_controller_uno
-arduino-cli upload --fqbn arduino:avr:uno -p /dev/ttyACM1 fan_controller_uno
